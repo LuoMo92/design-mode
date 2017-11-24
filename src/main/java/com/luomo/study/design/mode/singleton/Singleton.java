87a8228a754da6b126c65a4d6c7fc2a5998d14d7
LuoMo92 <810754420@qq.com>
2017-11-24 15:25:49 +0800

    Initial commit

---
+++ src/main/java/com/luomo/study/design/mode/singleton/Singleton.java
@@ -0,0 +1,30 @@
+package com.luomo.study.design.mode.singleton;
+
+/**
+ * @author LiuMei
+ * @date 2017-11-24.
+ */
+public class Singleton {
+
+    /**
+     * 一个静态的实例
+     */
+    private static Singleton singleton;
+
+    /**
+     * 私有化构造函数
+     */
+    private Singleton(){}
+
+    /**
+     * 给出一个公共的静态方法返回一个单一实例
+     * @return
+     */
+    public static Singleton getInstance(){
+        if(singleton == null){
+            singleton = new Singleton();
+        }
+        return singleton;
+    }
+
+}
---
+++ src/main/java/com/luomo/study/design/mode/test.java
@@ -0,0 +1,8 @@
+package com.luomo.study.design.mode;
+
+/**
+ * @author LiuMei
+ * @date 2017-11-24.
+ */
+public class test {
+}
