e4665c735e5ad6aa340b639edefaf310f3aad326
LuoMo92 <810754420@qq.com>
2018-05-17 15:24:29 +0800

    代理模式

---
+++ src/main/java/com/luomo/study/design/mode/proxy/Test.java
@@ -0,0 +1,16 @@
+package com.luomo.study.design.mode.proxy;
+
+import java.sql.Connection;
+
+/**
+ * @author LiuMei
+ * @date 2017-11-27.
+ */
+public class Test {
+
+    public static void main(String[] args) throws Exception {
+        Connection connection = DataSource.getInstance().getConnection();
+        connection.close();
+    }
+
+}
--- src/main/java/com/luomo/study/design/mode/test.java
+++
@@ -1,8 +0,0 @@
-package com.luomo.study.design.mode;
-
-/**
- * @author LiuMei
- * @date 2017-11-24.
- */
-public class test {
-}
